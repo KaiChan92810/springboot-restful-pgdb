478c6e4933cf6a99f95cd11dfcfd8b99a3a6eb10
zkdltid <zkdltid92810@gmail.com>
2021-10-19 18:15:01 +0800

    format

--- src/main/java/com/example/demo/dto/UserDto.java
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -1,33 +1,24 @@
 package com.example.demo.dto;
 
 import javax.validation.constraints.Email;
-
-//import java.util.Date;
-
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
-import lombok.Data; //get, set
-//import javax.
+import lombok.Data;
 
 @Data
 public class UserDto {
-    private long id;
-//    private Date creatAt;
-//    private Date updateAt;
-  
-    private String username;
+	private long id;
+	private String username;
+	@NotNull(message = "Email Can Not Be Null")
+	@Email(message = "Email Format Error")
+	private String email;
 
-    @NotNull(message = "Email Can Not Be Null")
-    @Email(message = "Email Format Error")
-    private String email;
-    
-    @NotNull(message = "Phone Can Not Be Null")
-    private String phone;
-    
-    @NotNull(message = "Password Can Not Be Null")
-    @Length(min = 6, max = 20, message = "Password Length Error")
-    private String password;    
-//    private Date updateAt;
+	@NotNull(message = "Phone Can Not Be Null")
+	private String phone;
+
+	@NotNull(message = "Password Can Not Be Null")
+	@Length(min = 6, max = 20, message = "Password Length Error")
+	private String password;
 }
