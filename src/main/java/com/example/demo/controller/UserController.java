2e11e2b8c9b8f855c13bafa7756fcdfca56cf116
zkdltid <zkdltid92810@gmail.com>
2021-10-19 18:13:21 +0800

    add error handler

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,23 +6,20 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-//import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.UserDto;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 //@CrossOrigin(origins = "http://localhost:8081")
 @RestController
@@ -30,19 +27,18 @@ import com.example.demo.repository.UserRepository;
 public class UserController {
 	@Autowired
 	UserRepository userRepository;
-	
-	
+
 	@GetMapping("/users")
-	public ResponseEntity<List<User>> getAllUser(){
-		try{
+	public ResponseEntity<List<User>> getAllUser() {
+		try {
 			List<User> users = new ArrayList<User>();
 			userRepository.findAll().forEach(users::add);
 			return new ResponseEntity<>(users, HttpStatus.OK);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	@GetMapping("/users/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
 		Optional<User> userData = userRepository.findById(id);
@@ -53,22 +49,16 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	@PostMapping("/users")
-	public ResponseEntity<User> creatUser(@Valid @RequestBody UserDto user) {
-		try {
-			User newUser = new User();
-			newUser.setUsername(user.getUsername());
-			newUser.setPassword(user.getPassword());
-			newUser.setPhone(user.getPhone());
-			newUser.setEmail(user.getEmail());
-			User _user = userRepository
-					.save(newUser);
-			return new ResponseEntity<>(_user, HttpStatus.CREATED);
-		} catch (Exception e) {
-			System.out.println(e);
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+	public ResponseEntity<User> creatUser(@Valid @RequestBody UserDto user) throws JsonProcessingException {
+		User newUser = new User();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(user.getPassword());
+		newUser.setPhone(user.getPhone());
+		newUser.setEmail(user.getEmail());
+		User _user = userRepository.save(newUser);
+		return new ResponseEntity<>(_user, HttpStatus.CREATED);
 	}
-	
+
 }
