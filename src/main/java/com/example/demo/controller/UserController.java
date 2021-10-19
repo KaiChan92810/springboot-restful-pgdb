a57cea379b31fff98ef8ef058f4fc23ab28f3588
zkdltid <zkdltid92810@gmail.com>
2021-10-19 11:40:42 +0800

    use user dto

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,22 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+//import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.repository.UserRepository;
+import com.example.demo.dto.UserDto;
 import com.example.demo.model.User;
-
+import com.example.demo.repository.UserRepository;
 
 //@CrossOrigin(origins = "http://localhost:8081")
 @RestController
@@ -27,6 +30,7 @@ import com.example.demo.model.User;
 public class UserController {
 	@Autowired
 	UserRepository userRepository;
+	
 	
 	@GetMapping("/users")
 	public ResponseEntity<List<User>> getAllUser(){
@@ -51,12 +55,18 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public ResponseEntity<User> creatUser(@RequestBody User user){
+	public ResponseEntity<User> creatUser(@Valid @RequestBody UserDto user) {
 		try {
+			User newUser = new User();
+			newUser.setUsername(user.getUsername());
+			newUser.setPassword(user.getPassword());
+			newUser.setPhone(user.getPhone());
+			newUser.setEmail(user.getEmail());
 			User _user = userRepository
-					.save(new User(user.getUsername(),user.getPhone(),user.getEmail()));
-			return new ResponseEntity<User>(_user, HttpStatus.CREATED);
+					.save(newUser);
+			return new ResponseEntity<>(_user, HttpStatus.CREATED);
 		} catch (Exception e) {
+			System.out.println(e);
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
